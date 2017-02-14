48d5305faaeb7733f9e9401558117afeb28e97f0
Peioris <mfmayol@up.edu.ph>
2017-02-14 15:41:58 +0800

    developed cookiecutter template

---
+++ ngse/ngse/__init__.py
@@ -0,0 +1,11 @@
+"""Main entry point
+"""
+from pyramid.config import Configurator
+
+
+def main(global_config, **settings):
+    config = Configurator(settings=settings)
+    config.include("cornice")
+    config.scan("ngse.views")
+    return config.make_wsgi_app()
+
