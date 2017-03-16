b0b211f31a5b0b88e00e1415cc1fe201e3b3fc64
Coarse <mfmayol@up.edu.ph>
2017-03-16 16:50:43 +0800

    removed unneeded files

--- load_data.sql
+++
@@ -1,2 +0,0 @@
-COPY categories(category_id, category, form_type_id) FROM '/Users/Peioris/Projects/cs192/csvs/categories.csv' with delimiter as ',' csv;
-COPY questions(question_id, question, form_type_id, category_id) FROM '/Users/Peioris/Projects/cs192/csvs/questions.csv' with delimiter as ',' csv;
