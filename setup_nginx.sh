65d6a234fe8cb52b5c8e603abda60ffd695dbc7f
Coarse <mfmayol@up.edu.ph>
2017-05-25 09:26:08 +0800

    added more scripts

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo rm /etc/nginx/nginx.conf;
-sudo cp nginx.conf /etc/nginx/nginx.conf;
+sudo cp /home/peioris/NGSE/nginx.conf /etc/nginx/nginx.conf;
 sudo service nginx stop;
 sudo service nginx start;
 
-gunicorn --paste ngse.ini -b 0.0.0.0:8080 --workers=3 &;
+gunicorn --paste /home/peioris/NGSE/ngse.ini -b 0.0.0.0:8080 --workers=3 &;
